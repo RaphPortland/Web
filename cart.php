e7a3b89737b2f97dc6f9f3112b72db77505d9a27
RaphPortland <raphaelpartouche@gmail.com>
2019-05-02 15:39:35 +0200

    Add files via upload

--- cart.php
+++ cart.php
@@ -12,8 +12,54 @@ if($_GET["action"]=="supp"){
 	$_SESSION['newvaleueonthecart'] = $workwith;
 
 
-}
-else {
+} elseif ($_GET["action"]=="supprimeritems"){
+
+				define('DB_SERVER', 'localhost');
+				define ('DB_USER', 'root');
+				define ('DB_PASS', 'root');
+
+				// identifier le nom de la base de données 
+
+				$database = "Projetweb";
+				//connecter l'utilsateur dans la BDD
+				$db_handle = mysqli_connect (DB_SERVER, DB_USER, DB_PASS);
+				$db_found = mysqli_select_db ($db_handle, $database);
+				$newStock = 0;
+
+				// si la BDD existe, faire le traitement
+
+				    if ($db_found) {
+				        $sql = "SELECT * FROM Items WHERE Id ='".$_GET["id"]."'";
+				        $result = mysqli_query ($db_handle, $sql);
+				            while ($data = mysqli_fetch_assoc($result)){
+
+				            	$newStock = $data["Stock"]-1; 
+				            	if($newStock == 0){
+				            		$sql2 = "DELETE FROM Items WHERE Id ='".$_GET["id"]."'";
+				            		$result2 = mysqli_query ($db_handle, $sql2);
+				            		header('Location: itemsenvente.php');
+
+				            	} else {
+				            		$sql23 = "UPDATE Items SET Stock ='".$newStock."' WHERE Id ='".$_GET["id"]."'";
+				            		$result23 = mysqli_query ($db_handle, $sql23);
+				            			header('Location: itemsenvente.php');
+
+				            	}
+
+				           
+				            }//end while
+				    }//end if
+
+				// si la BDD n'existe pas 
+				    else {
+				        echo 'Database not found';
+				    }
+
+				// Fermer la connection 
+				mysqli_close($db_handle);
+
+
+} else {
 	if (isset($_SESSION['newvaleueonthecart'])) {
 
 
